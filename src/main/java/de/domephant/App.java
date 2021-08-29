43a24747e7be6478889a4c64df2110f271a6a63d
Domephant <domephant@web.de>
2021-08-29 11:18:07 +0200

    initial Commit

---
+++ src/main/java/de/domephant/App.java
@@ -0,0 +1,15 @@
+package de.domephant;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
