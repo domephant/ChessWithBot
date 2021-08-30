afe05a20c93b638a5459f333a23b012b3ccbe181
Domephant <domephant@web.de>
2021-08-30 22:05:45 +0200

    Added chess pieces to the board

--- src/main/java/de/domephant/App.java
+++ src/main/java/de/domephant/App.java
@@ -1,16 +1,20 @@
 package de.domephant;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.Console;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 public class App extends JFrame {
     public final Panel[][] grid = new Panel[8][8];
     private static String player1;
     private static String player2;
 
-    public App() {
+    public App() throws IOException {
         this.setLayout(new GridLayout(8, 8));
         this.setSize(900, 900);
         for (int i = 0; i < 8; i++) {
@@ -18,13 +22,13 @@ public class App extends JFrame {
                 grid[i][j] = new Panel();
                 if ((i*8 + j + 1) % 2 == 0) {
                     if(i % 2 == 0) {
-                        grid[i][j].setBackground(Color.BLACK);
+                        grid[i][j].setBackground(Color.GRAY);
                     } else {
                         grid[i][j].setBackground(Color.WHITE);
                     }
                 } else {
                     if(i % 2 != 0) {
-                        grid[i][j].setBackground(Color.BLACK);
+                        grid[i][j].setBackground(Color.GRAY);
                     } else {
                         grid[i][j].setBackground(Color.WHITE);
                     }
@@ -32,11 +36,54 @@ public class App extends JFrame {
                 this.add(grid[i][j]);
             }
         }
+        //Pawn Spawning
+        BufferedImage img_black = ImageIO.read(new File("src/main/resources/assets/black/png/Chess_pdt60.png"));
+        BufferedImage img_white = ImageIO.read(new File("src/main/resources/assets/white/png/Chess_plt60.png"));
+        for(int h = 0; h < 8; h++) {
+            grid[1][h].add(new JLabel(new ImageIcon(img_black)));
+            grid[6][h].add(new JLabel(new ImageIcon(img_white)));
+        }
+
+        //Rook Spawning
+        //Black
+        grid[0][0].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_rdt60.png")))));
+        grid[0][7].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_rdt60.png")))));
+        //White
+        grid[7][0].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_rlt60.png")))));
+        grid[7][7].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_rlt60.png")))));
+
+        //Knight Spawning
+        //Black
+        grid[0][1].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_ndt60.png")))));
+        grid[0][6].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_ndt60.png")))));
+        //White
+        grid[7][1].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_nlt60.png")))));
+        grid[7][6].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_nlt60.png")))));
+
+        //Bishop Spawning
+        //Black
+        grid[0][2].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_bdt60.png")))));
+        grid[0][5].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_bdt60.png")))));
+        //White
+        grid[7][2].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_blt60.png")))));
+        grid[7][5].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_blt60.png")))));
+
+        //Queen Spawning
+        //Black
+        grid[0][3].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_qdt60.png")))));
+        //White
+        grid[7][3].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_qlt60.png")))));
+
+        //King Spawning
+        //Black
+        grid[0][4].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/black/png/Chess_kdt60.png")))));
+        //White
+        grid[7][4].add(new JLabel(new ImageIcon(ImageIO.read(new File("src/main/resources/assets/white/png/Chess_klt60.png")))));
+
         this.setVisible(true);
     }
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         System.out.println("Enter start parameters: ");
         checkParameters(new Scanner(System.in).nextLine());
         new App();
